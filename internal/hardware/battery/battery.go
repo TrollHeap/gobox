3f2c39290217291901d0aa8d901a5d986b33e2b7
trollheap <codegrunt@proton.me>
2025-10-20 15:58:19 +0200

    feat: Add battery package for reading battery information and print functionality

---
+++ internal/hardware/battery/battery.go
@@ -0,0 +1,62 @@
+package battery
+
+import (
+	"errors"
+	"os"
+)
+
+type BatteryInfo struct {
+	Status       string
+	Manufacturer string
+	Model        string
+	Serial       string
+	Technology   string
+	Capacity     int
+	Cycle        int
+	EnergyAH     float64
+	VoltageNow   float64
+	EnergyFull   float64
+}
+
+func convertWattToAmpere(path string, energyFull float64) float64 {
+	voltDesign, _ := readFloat(path, "voltage_min_design")
+
+	mAH := (energyFull / 1_000_000) * 1000 / (voltDesign / 1_000_000)
+	return mAH
+}
+
+func GetBatteryInfo() (BatteryInfo, error) {
+	path, err := findBatteryPath()
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return BatteryInfo{Capacity: -1, Status: "N/A"}, nil
+		}
+		return BatteryInfo{}, err
+	}
+
+	capacity, _ := readInt(path, "capacity")
+	cycle, _ := readInt(path, "cycle_count")
+	status, _ := readString(path, "status")
+	manufacturer, _ := readString(path, "manufacturer")
+	model, _ := readString(path, "model_name")
+	serial, _ := readString(path, "serial_number")
+	technology, _ := readString(path, "technology")
+	volt, _ := readFloat(path, "voltage_now")
+	energyFull, _ := readFloat(path, "energy_full_design")
+	energyAH := convertWattToAmpere(path, energyFull)
+
+	return BatteryInfo{
+		Status:       status,
+		Manufacturer: manufacturer,
+		Model:        model,
+		Serial:       serial,
+		Technology:   technology,
+		Capacity:     capacity,
+		Cycle:        cycle,
+		EnergyAH:     energyAH,
+		VoltageNow:   volt,
+		EnergyFull:   energyFull,
+	}, nil
+}
+
+// Technology, capacity level
---
+++ internal/hardware/battery/battery_sysfs.go
@@ -0,0 +1,47 @@
+package battery
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func findBatteryPath() (string, error) {
+	entries, err := os.ReadDir("/sys/class/power_supply")
+	if err != nil {
+		return "", err
+	}
+	for _, entry := range entries {
+		name := entry.Name()
+		if strings.HasPrefix(name, "BAT") {
+			return filepath.Join("/sys/class/power_supply", name), nil
+		}
+	}
+	return "", fmt.Errorf("no battery found")
+}
+
+func readInt(batteryPath string, filename string) (int, error) {
+	data, err := os.ReadFile(filepath.Join(batteryPath, filename))
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(strings.TrimSpace(string(data)))
+}
+
+func readFloat(path, filename string) (float64, error) {
+	data, err := os.ReadFile(filepath.Join(path, filename))
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseFloat(strings.TrimSpace(string(data)), 64)
+}
+
+func readString(batteryPath string, filename string) (string, error) {
+	data, err := os.ReadFile(filepath.Join(batteryPath, filename))
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(data)), nil
+}
