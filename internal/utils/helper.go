a7aae72942d4e1e75eaa88013583b558fff45c7f
trollheap <codegrunt@proton.me>
2025-10-30 19:49:29 +0100

    refactor(utils): Update function parameter name and improve variable naming in ConvertWattToAmpere function

--- internal/utils/helper.go
+++ internal/utils/helper.go
@@ -2,16 +2,19 @@ package utils
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"gobox/internal/sysfs"
 )
 
-func ConvertWattToAmpere(path string, energyFull float64) (float64, error) {
-	voltDesign, err := sysfs.ReadFloat(path, "voltage_min_design")
+func ConvertWattToAmpere(batteryPath string, energyFull float64) (float64, error) {
+	voltagePath := filepath.Join(batteryPath, "voltage_min_design")
+
+	voltDesign, err := sysfs.ReadFloat(voltagePath)
 	if err != nil {
 		return 0, fmt.Errorf("reading voltage: %w", err)
 	}
 
-	mAH := (energyFull / 1_000_000) * 1000 / (voltDesign / 1_000_000)
-	return mAH, nil
+	mAh := (energyFull / 1_000_000) * 1000 / (voltDesign / 1_000_000)
+	return mAh, nil
 }
