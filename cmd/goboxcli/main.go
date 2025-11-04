582e334802a3060de01857d542c6bbb43a379149
trollheap <codegrunt@proton.me>
2025-11-04 15:53:20 +0100

    feat: Add Bubble Tea package for interactive CLI interface and update main.go with model structure and methods

--- cmd/goboxcli/main.go
+++ cmd/goboxcli/main.go
@@ -1,9 +1,50 @@
 package main
 
-import printHw "gobox/internal/print_hw"
+import (
+	"fmt"
+	"os"
+
+	display "gobox/internal/ui/display"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	count int
+}
+
+func NewModel() model {
+	return model{count: 0}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.count++
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Count: %d\n", m.count)
+}
 
 func main() {
 	// print.CPUInfo()
-	printHw.DisplayBatteryReport()
+	display.DisplayRamInfo()
 	// print.DiskInfo()
+	p := tea.NewProgram(NewModel())
+	if err := p.Start(); err != nil {
+		fmt.Println("Erreur:", err)
+		os.Exit(1)
+	}
 }
